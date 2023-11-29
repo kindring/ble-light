cfe488e45b079b3b7aa8985a339883348dd8f409
kindring <2460131004@qq.com>
2023-11-29 22:03:11 +0800

    t

--- src/light.h
+++ src/light.h
@@ -1,11 +1,19 @@
+/*
+ * @Author: kindring 2460131004@qq.com
+ * @Date: 2023-11-20 23:37:29
+ * @LastEditors: kindring 2460131004@qq.com
+ * @LastEditTime: 2023-11-25 21:37:47
+ * @FilePath: \ble-light\src\light.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 #ifndef __LIGHT_H__
 #define __LIGHT_H__
 
 // 暖光 pwm脚
-#define GPIO_WARM P24
+#define GPIO_WARM P18
 // 冷光 pwm脚
-#define GPIO_COLD P25
+#define GPIO_COLD P20
 
 // 风扇 pwm
 #define GPIO_FAN P23
